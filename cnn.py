8c89c98b4d9fe6dcbc1428193b600c473d4e0db7
emceelamb <mlam@berkeley.edu>
2018-11-26 19:58:55 -0500

    add python scripts

---
+++ cnn.py
@@ -0,0 +1,5 @@
+import os
+
+cnn_url="http://tunein.streamguys1.com/cnn?aw_0_1st.playerid=RadioTime&aw_0_1st.skey=15432794849cb6503cb3adbcaed7ff7a42f7"
+
+os.system('sudo mpg123 '+fox_url)
---
+++ fox.py
@@ -0,0 +1,5 @@
+import os
+
+fox_url="http://tunein.streamguys1.com/secure-foxnews?key=30c6a0f4051091d05642a8178d21f17151b61e9cb6503cb3adbcaed7ff7a42f7"
+
+os.system('sudo mpg123 '+fox_url)
