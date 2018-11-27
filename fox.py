2c0572fb580fb381644d427ba96c93a4062fcd2d
emceelamb <mlam@berkeley.edu>
2018-11-27 01:58:41 +0000

    sending to laptop

--- fox.py
+++ fox.py
@@ -1,5 +1,15 @@
 import os
+import subprocess
+import time
 
 fox_url="http://tunein.streamguys1.com/secure-foxnews?key=30c6a0f4051091d05642a8178d21f17151b61e9cb6503cb3adbcaed7ff7a42f7"
 
-os.system('sudo mpg123 '+fox_url)
+#os.system('sudo mpg123 -q '+fox_url)
+#os.popen('sudo mpg123 ' + fox_url + ' &')
+subprocess.Popen(['sudo','mpg123',fox_url])
+time.sleep(1)
+#time.sleep(10)
+serial.write('\x03')
+
+
+
