6ee0d175d72c45c0104447b6fa9551c1bb4ce977
emceelamb <mlam@berkeley.edu>
2018-11-27 01:10:29 +0000

    fix cnn url again

--- cnn.py
+++ cnn.py
@@ -1,5 +1,5 @@
 import os
 
-cnn_url="http://tunein.streamguys1.com/cnn?aw_0_1st.playerid=RadioTime&aw_0_1st.skey=15432794849cb6503cb3adbcaed7ff7a42f7"
+cnn_url="http://tunein.streamguys1.com/cnn?skey=2547892960cb6503cb3adbcaed7ff7a42f7"
 
 os.system('sudo mpg123 '+cnn_url)
