13c1676051263d472cf20a55f55d3ac3b5a83154
emceelamb <mlam@berkeley.edu>
2018-11-28 16:55:55 -0500

    add speaker state

--- radRadio.py
+++ radRadio.py
@@ -17,6 +17,9 @@ GPIO.setup(24, GPIO.OUT)
 fox_url="http://tunein.streamguys1.com/secure-foxnews?key=197d25655542f4c4e8d9c1eb15b264b81093805bec3ebed5572706526d919fee"
 cnn_url="http://tunein.streamguys1.com/cnn?skey=2547892960cb6503cb3adbcaed7ff7a42f7"
 
+cnnIsPlaying=False
+foxIsPlaying=False
+
 while True: 
     # toggle stations
     fox = GPIO.input(18)
@@ -24,20 +27,28 @@ while True:
     # power light
     GPIO.output(22,GPIO.HIGH) 
     if fox == True:
+        if(cnnIsPlaying==True):
+            os.system('sudo killall mpg123')
         # turn blue light off
         GPIO.output(24,GPIO.LOW)
         # turn red light on
         GPIO.output(23,GPIO.HIGH)
-        subprocess.Popen(['sudo','mpg123',fox_url])
-        print('fox is playing')
+        if(foxIsPlaying==False):
+            foxIsPlaying = True
+            subprocess.Popen(['sudo','mpg123',fox_url])
+            print('fox is playing')
         time.sleep(1)
     if cnn == True:
+        if(foxIsPlaying==True):
+            os.system('sudo killall mpg123')
         # turn red light off
         GPIO.output(23,GPIO.LOW)
         # turn blue light ON
-        print('cnn play') 
-        subprocess.Popen(['sudo','mpg123',cnn_url])
         GPIO.output(24,GPIO.HIGH)
+        if(cnnIsPlaying==False):
+            cnnIsPlaying=True
+            subprocess.Popen(['sudo','mpg123',cnn_url])
+            print('cnn play') 
         time.sleep(1)
 
 
