7a66ab7b37a684f1374d8cfcecce8abc1f3f6a20
emceelamb <mlam@berkeley.edu>
2018-11-30 22:45:12 -0500

    changed rad radio fox to match cnn

--- radRadio.py
+++ radRadio.py
@@ -25,31 +25,55 @@ while True:
     # toggle stations
     fox = GPIO.input(18)
     cnn = GPIO.input(15)
+
     # power light
     GPIO.output(22,GPIO.HIGH) 
+
+    # if fox == True:
+    #     if(cnnIsPlaying==True):
+    #         os.system('sudo killall mpg123')
+
+    #     # turn blue light off
+    #     GPIO.output(23,GPIO.LOW)
+    #     # turn red light on
+    #     GPIO.output(24,GPIO.HIGH)
+        
+    #     if(foxIsPlaying==False):
+	#         cnnIsPlaying=False
+    #         foxIsPlaying=True
+	    
+    #         subprocess.Popen(['sudo','mpg123',fox_url])
+
+
+    #     time.sleep(1)
     if fox == True:
         if(cnnIsPlaying==True):
             os.system('sudo killall mpg123')
-        # turn blue light off
-        GPIO.output(23,GPIO.LOW)
-        # turn red light on
-        GPIO.output(24,GPIO.HIGH)
-        if(foxIsPlaying==False):
-	    cnnIsPlaying=False
-            foxIsPlaying=True
-	    
-            subprocess.Popen(['sudo','mpg123',fox_url])
-            print('fox is playing')
-        time.sleep(1)
-    if cnn == True:
-        if(foxIsPlaying==True):
-            os.system('sudo killall mpg123')
+
         # turn red light off
         GPIO.output(24,GPIO.LOW)
         # turn blue light ON
         GPIO.output(23,GPIO.HIGH)
+
+        if(foxIsPlaying==False):
+            cnnIsPlaying=False
+            foxIsPlaying=True
+            subprocess.Popen(['sudo','mpg123',fox_url])
+            print('fox play') 
+        time.sleep(1)
+
+
+    if cnn == True:
+        if(foxIsPlaying==True):
+            os.system('sudo killall mpg123')
+
+        # turn red light off
+        GPIO.output(24,GPIO.LOW)
+        # turn blue light ON
+        GPIO.output(23,GPIO.HIGH)
+
         if(cnnIsPlaying==False):
-	    foxIsPlaying=False
+            foxIsPlaying=False
             cnnIsPlaying=True
             subprocess.Popen(['sudo','mpg123',cnn_url])
             print('cnn play') 
